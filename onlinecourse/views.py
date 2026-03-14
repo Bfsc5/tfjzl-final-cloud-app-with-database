fe2a5a7cac7ec8fa11a5b53f873d592442a4d3ea
Bfsc5 <stevencruz5@hotmail.com>
2026-03-14 16:03:49 +0000

    Refactor submit and show_exam_result functions

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -112,12 +112,15 @@ def enroll(request, course_id):
          # Redirect to show_exam_result with the submission id
 #def submit(request, course_id):
 def submit(request, course_id):      
-    course = get_object_or_404(Course, pk=course_id)
-    user = request.user enrollment = Enrollment.objects.get(user=user, course=course)
+    user = request.user 
+    course = get_object_or_404(
+        Course, pk=course_id)
+    enrollment = Enrollment.objects.get(user=user, course=course)
     submission = Submission.objects.create(enrollment=enrollment)
     choices = extract_answers(request)
     submission.choices.set(choices)
-    submission_id = submission.id return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
+    submission_id = submission.id 
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
 
 
@@ -139,14 +142,34 @@ def extract_answers(request):
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
 
-ef show_exam_result(request, course_id, submission_id): context = {} course = get_object_or_404(Course, pk=course_id) submission = Submission.objects.get(id=submission_id) choices = submission.choices.all()
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
 
-total_score = 0 questions = course.question_set.all() # Assuming course has related questions
+    score = 0
+    total = 0
 
-for question in questions: correct_choices = question.choice_set.filter(is_correct=True) # Get all correct choices for the question selected_choices = choices.filter(question=question) # Get the user's selected choices for the question
+    for question in course.question_set.all():
+        total += question.grade
+        if question.is_get_score(choices):
+            score += question.grade
+
+    context = {
+        'course': course,
+        'selected_choices': choices,
+        'grade': score,
+        'total': total
+    }
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+     # Get all correct choices for the question selected_choices = choices.filter(question=question) # Get the user's selected choices for the question
 
 # Check if the selected choices are the same as the correct choices if set(correct_choices) == set(selected_choices): total_score += question.grade # Add the question's grade only if all correct answers are selected
 
-context['course'] = course context['grade'] = total_score context['choices'] = choices
-
-return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+    context = {
+        'course': course,
+        'selected_choices': choices,
+        'grade': score,
+        'total': total
+    }
